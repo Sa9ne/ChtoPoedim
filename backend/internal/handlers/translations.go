d2c494db47a1dfd0320caa00bf89305cfdb2d5cb
Sa9ne <trealev@gmail.com>
2025-03-12 19:30:03 +0500

    bug fixed

--- backend/internal/handlers/translations.go
+++ backend/internal/handlers/translations.go
@@ -11,11 +11,8 @@ import (
 func GetTranslations(ctx *gin.Context) {
 	lang := ctx.DefaultQuery("lang", "en")
 
-	fmt.Printf("Запрос переводов для языка: %s\n", lang)
-
 	translations, err := localization.GetTranslations(lang)
 	if err != nil {
-		fmt.Printf("Ошибка загрузки переводов: %v\n", err) // Вывод в консоль сервера
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Ошибка загрузки переводов: %v", err)})
 		return
 	}
--- backend/internal/localization/localization.go
+++ backend/internal/localization/localization.go
@@ -8,42 +8,35 @@ import (
 
 var translations = make(map[string]map[string]string)
 
-// Загрузка переводов из файлов
 func LoadTranslations() error {
 	languages := []string{"en", "ru"}
 
 	for _, lang := range languages {
 		filePath := fmt.Sprintf("D:/important/Let's Eat/backend/internal/localization/%s.json", lang)
-		fmt.Printf("Загружаем файл: %s\n", filePath)
 
 		file, err := os.Open(filePath)
 		if err != nil {
-			fmt.Printf("Ошибка открытия файла: %v\n", err)
-			return fmt.Errorf("Ошибка открытия файла %s: %w", filePath, err)
+			return fmt.Errorf("ошибка открытия файла %s: %w", filePath, err)
 		}
 		defer file.Close()
 
 		var data map[string]string
 		if err := json.NewDecoder(file).Decode(&data); err != nil {
-			fmt.Printf("Ошибка декодирования JSON: %v\n", err)
-			return fmt.Errorf("Ошибка декодирования JSON %s: %w", filePath, err)
+			return fmt.Errorf("ошибка декодирования JSON %s: %w", filePath, err)
 		}
+
 		translations[lang] = data
 		fmt.Printf("Файл %s загружен успешно!\n", filePath)
 	}
 
-	fmt.Println("Текущие переводы:", translations)
 	return nil
 }
 
 func GetTranslations(lang string) (map[string]string, error) {
-	fmt.Printf("Ищем переводы для языка: %s\n", lang)
 
 	if data, exists := translations[lang]; exists {
-		fmt.Println("Переводы найдены!")
 		return data, nil
 	}
 
-	fmt.Println("Переводы не найдены!")
-	return nil, fmt.Errorf("Переводы для языка %s не найдены", lang)
+	return nil, fmt.Errorf("переводы для языка %s не найдены", lang)
 }
