93406d1140c640079c1cf45552f48127d9c90d0d
Sa9ne <trealev@gmail.com>
2025-03-12 17:30:44 +0500

    Try to do more languages

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"LetsEat/backend/internal/database"
 	"LetsEat/backend/internal/handlers"
+	"LetsEat/backend/internal/middlewares"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -17,9 +18,13 @@ func Start() {
 	// Работа статической директории frontend
 	s.Static("/frontend", "D:/important/Let's Eat/frontend")
 
+	// Ставим middlewares для проверки языка
+	s.Use(middlewares.LangCheck)
+
 	// Маршрутизаторы
 	s.GET("/", handlers.WelcomeReq)
 	s.GET("/search", handlers.SearchRecipes)
+	s.GET("/set-languages")
 
 	err := s.Run(":8080")
 	if err != nil {
