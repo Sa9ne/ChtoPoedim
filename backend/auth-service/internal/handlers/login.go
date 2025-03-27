70139669b2625040c0fe90bc1930d006b41f0098
Sa9ne <trealev@gmail.com>
2025-03-27 16:43:18 +0500

    login handler

--- backend/auth-service/internal/handlers/login.go
+++ backend/auth-service/internal/handlers/login.go
@@ -1,7 +1,25 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"LetsEat/backend/auth-service/internal/database"
+	"LetsEat/backend/auth-service/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Login(ctx *gin.Context) {
+	var user models.Users
 
+	if err := ctx.Bind(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "struct user not found"})
+		return
+	}
+
+	if err := database.DB.Where("email = ? AND password = ?", user.Email, user.Password).First(&user).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Email or Password input invalid"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"Message": "Log In was successful!"})
 }
