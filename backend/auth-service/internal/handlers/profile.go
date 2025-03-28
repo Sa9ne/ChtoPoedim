4081afcf9d719254307349c5a872d4fbcb95dfeb
Sa9ne <trealev@gmail.com>
2025-03-29 00:32:30 +0500

    profile request

--- backend/auth-service/internal/handlers/profile.go
+++ backend/auth-service/internal/handlers/profile.go
@@ -1,7 +1,30 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"LetsEat/backend/auth-service/internal/database"
+	"LetsEat/backend/auth-service/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Profile(ctx *gin.Context) {
-	//s
+	var user models.Users
+
+	userID := ctx.GetHeader("user")
+	if userID == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Don't have user header"})
+		return
+	}
+
+	if err := database.DB.Where("id = ?", userID).First(&user).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Can't find user"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"id":    user.ID,
+		"name":  user.Name,
+		"email": user.Email,
+	})
 }
