def492babdcc4e72becd7a176f9eed51cbe2e8a1
Sa9ne <trealev@gmail.com>
2025-03-29 13:28:26 +0500

    comment for login

--- backend/auth-service/internal/handlers/login.go
+++ backend/auth-service/internal/handlers/login.go
@@ -12,17 +12,20 @@ import (
 func Login(ctx *gin.Context) {
 	var input models.Users
 
+	// Парсим данные
 	if err := ctx.Bind(&input); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "struct user not found"})
 		return
 	}
 
+	// Проверяем почту
 	var user models.Users
 	if err := database.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
 		ctx.JSON(http.StatusNotFound, gin.H{"error": "Email input invalid"})
 		return
 	}
 
+	// Проверяем пароль
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Password input invalid"})
