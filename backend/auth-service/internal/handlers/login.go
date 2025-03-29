83baca442c1e12e0420fa603c221b8042bbdc130
Sa9ne <trealev@gmail.com>
2025-03-29 13:26:17 +0500

    Now password will hashed

--- backend/auth-service/internal/handlers/login.go
+++ backend/auth-service/internal/handlers/login.go
@@ -6,18 +6,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Login(ctx *gin.Context) {
-	var user models.Users
+	var input models.Users
 
-	if err := ctx.Bind(&user); err != nil {
+	if err := ctx.Bind(&input); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "struct user not found"})
 		return
 	}
 
-	if err := database.DB.Where("email = ? AND password = ?", user.Email, user.Password).First(&user).Error; err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{"error": "Email or Password input invalid"})
+	var user models.Users
+	if err := database.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Email input invalid"})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Password input invalid"})
 		return
 	}
 
--- backend/auth-service/internal/handlers/register.go
+++ backend/auth-service/internal/handlers/register.go
@@ -6,16 +6,41 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(ctx *gin.Context) {
-	var user models.Users
+	var input models.Users
 
-	if err := ctx.Bind(&user); err != nil {
+	// Парсим данные
+	if err := ctx.Bind(&input); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Struct doesn't work"})
 		return
 	}
 
+	// Проверяем был ли зарегистрирован данный email
+	var existingUser models.Users
+	if err := database.DB.Where("email = ?", input.Email).First(&existingUser).Error; err == nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "This email was be registered"})
+		return
+	}
+
+	// Хешируем пароль
+	cost := 10
+	HashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), cost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Password was not hashed"})
+		return
+	}
+
+	// Создаем структуру, где уже пароль захеширован
+	user := models.Users{
+		Name:     input.Name,
+		Email:    input.Email,
+		Password: string(HashedPassword),
+	}
+
+	// Создаем пользователя в БД, со структурой user
 	if err := database.DB.Create(&user).Error; err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "User doesn't create"})
 		return
