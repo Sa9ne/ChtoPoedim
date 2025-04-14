1af904d8173e3c465bbfce7dea77fd8020476417
Sa9ne <trealev@gmail.com>
2025-04-14 22:55:06 +0500

    Catalog update, new modal page, new main design

--- frontend/js/catalog.js
+++ frontend/js/catalog.js
@@ -3,3 +3,8 @@ function OpenCatalog() {
   CloseModal(Catalog);
   Catalog.classList.toggle("hidden")
 }
+// Функция открытия окна блюда дня
+function OpenDayDish() {
+  CloseModal(DishDay);
+  DishDay.classList.toggle("hidden")
+}
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,9 +4,10 @@
     window.OpenLang = document.getElementById("lang-modal")
     window.OpenProf = document.getElementById("profile-form")
     window.Catalog = document.getElementById("catalog-form")
+    window.DishDay = document.getElementById("Dish-day")
 // Функция закрытия всех модальных окон
 function CloseModal(expect = null) {
-    const modals = [AuthForm, RegisterForm, OpenLang, OpenProf, Catalog];
+    const modals = [AuthForm, RegisterForm, OpenLang, OpenProf, Catalog, DishDay];
 
     modals.forEach(modal => {
         if (modal !== expect) {
