045f619fe3c424bdc0213a14d86e3b504d0c8df8
Sa9ne <trealev@gmail.com>
2025-05-03 12:02:16 +0500

    database struct update

--- backend/food-service/internal/server/server.go
+++ backend/food-service/internal/server/server.go
@@ -19,6 +19,7 @@ func Start() {
 	s.Static("/frontend", "/Users/user/important/ChtoPoedim/frontend")
 
 	s.GET("/DishDay", handlers.DishDay)
+	s.GET("/DishCatalog", handlers.Catalog)
 
 	err := s.Run(":8082")
 	if err != nil {
--- backend/main-service/internal/database/db.go
+++ backend/main-service/internal/database/db.go
@@ -16,7 +16,11 @@ type Dishes struct {
 	ID          uint   `gorm:"PrimaryKey"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
+	Img         string `json:"image"`
 	Time        int    `json:"time"`
+	Vegan       bool   `json:"vegan"`
+	Spicy       bool   `json:"spicy"`
+	Hearty      bool   `json:"hearty"`
 }
 
 func ConnectDB() {
